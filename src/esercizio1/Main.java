3662253766b487fcd00bf72aa4c1168318b57212
pierluigi991 <123975572+pierluigi991@users.noreply.github.com>
2023-05-08 15:28:59 +0200

    fix al quadrato

--- src/esercizio1/Main.java
+++ src/esercizio1/Main.java
@@ -52,13 +52,13 @@ public class Main {
 
    
     private static void stampaArray(int[] array) {
-        System.out.print("[");
+        
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
             if (i < array.length - 1) {
                 System.out.print(", ");
             }
         }
-        System.out.println("]");
+       
     }
 }
